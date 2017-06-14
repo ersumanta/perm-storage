b09bdbed909fdfd075d8425789bc9620c7f1ff88
Sumanta Sarkar <sumanta.sr@gmail.com>
2017-06-14 20:55:22 +0100

    Build 1.0.8

--- dist/index.js
+++ dist/index.js
@@ -7,10 +7,16 @@ module.exports = {
     set: function set(id, value) {
         localStorage.setItem(id, value);
     },
+    delete: function _delete(id) {
+        localStorage.removeItem(id);
+    },
     getObj: function get(id) {
         return JSON.parse(localStorage.getItem(id));
     },
     setObj: function set(id, value) {
         localStorage.setItem(id, JSON.stringify(value));
+    },
+    deleteObj: function deleteObj(id) {
+        localStorage.removeItem(id);
     }
 };

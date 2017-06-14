ab47687371ce872c7dfb3a697c7cc330b71dbdb5
Prashant Jaiswal <prashant.jaiswal@hotmail.com>
2017-06-15 00:27:37 +0530

    Updated file
    added delete item by by key

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ module.exports = {
     set: function set(id, value) {
         localStorage.setItem(id, value);
     },
+    delete: function(id) {
+        localStorage.removeItem(id)
+    },
     getObj: function get(id) {
         return JSON.parse(localStorage.getItem(id));
     },
     setObj: function set(id, value) {
         localStorage.setItem(id, JSON.stringify(value));
+    },
+    deleteObj: function(id) {
+        localStorage.removeItem(id)
     }
 };
